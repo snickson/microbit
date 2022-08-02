65a343114806a31dc9d3145705a4d0fd1660b394
Stephen N <stephen.nickson@gmail.com>
2022-08-02 21:09:14 +0100

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# Add your Python code here. E.g.
+while True:
+    basic.show_string("Penblwydd Hapus, Amelia!")
+    basic.pause(100)
+    for i in range(14):
+        basic.show_icon(IconNames.HEART)
+        basic.pause(100)
+        basic.clear_screen()
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+//  Add your Python code here. E.g.
 while (true) {
-    basic.showIcon(IconNames.Heart)
+    basic.showString("Penblwydd Hapus, Amelia!")
+    basic.pause(100)
+    for (let i = 0; i < 14; i++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.clearScreen()
+    }
 }
