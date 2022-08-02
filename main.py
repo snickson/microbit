5a828f063e54fe76697f670da3369a914e7e8e2e
Stephen N <stephen.nickson@gmail.com>
2022-08-02 21:12:57 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 # Add your Python code here. E.g.
 while True:
     basic.show_string("Penblwydd Hapus, Amelia!")
-    basic.pause(100)
+    basic.pause(500)
     for i in range(14):
         basic.show_icon(IconNames.HEART)
-        basic.pause(100)
+        basic.pause(500)
         basic.clear_screen()
+        basic.pause(500)
--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 //  Add your Python code here. E.g.
 while (true) {
     basic.showString("Penblwydd Hapus, Amelia!")
-    basic.pause(100)
+    basic.pause(500)
     for (let i = 0; i < 14; i++) {
         basic.showIcon(IconNames.Heart)
-        basic.pause(100)
+        basic.pause(500)
         basic.clearScreen()
+        basic.pause(500)
     }
 }
